6a0f01db22cd8cba5e52ae25b962ddba3e70152e
Nicolas Facchinetti <facchinetti.nicolas@outlook.it>
2022-04-26 17:24:00 +0200

    improved version 1: compute demand in costant time

--- order_no_price_constraint.py
+++ order_no_price_constraint.py
@@ -5,9 +5,6 @@ from progress_bar import progress
 
 import configparser
 
-def calc_demand(items, first, last):
-    # the first is -1 since the list start from 0 but in the formulation from 1
-    return sum(map(lambda i: i.demand, items.items[first-1: last]))
 
 # read configuration file
 config = configparser.ConfigParser()
@@ -34,11 +31,8 @@ print('Base cases creation...\n')
 for h in range(0, items.N):
     pairs[h+1,1] = {}
     pairs[h+1, 1]['z'] = (items.get_item(h).price - items.get_item(0).price)/2
-    d_h = calc_demand(items, 1, h+1)
+    d_h = items.items[h].cumdemand
     pairs[h+1, 1]['v'] = (items.get_item(h).price + items.get_item(0).price)/2 * d_h
-    pairs[h+1, 1]['s'] = [1]
-    pairs[h+1, 1]['e'] = [h+1]
-    pairs[h+1, 1]['q'] = [pairs[h+1, 1]['v']/d_h]
 
 
 printv(*(f'\tState {x[0]} -> {x[1]}' for x in pairs.items()), sep='\n')
@@ -79,19 +73,19 @@ for i in range(1, items.N+1):
             z_new = (p_last - p_first)/2
             z_old = pairs[j,k-1]['z']
             v_old = pairs[j,k-1]['v']
-            new_demand = calc_demand(items, j+1, i)
-            old_demand = calc_demand(items, 1, j)
+
+            old_demand =  items.items[j-1].cumdemand
+            new_demand = items.items[i-1].cumdemand - items.items[j-1].cumdemand
+
             # compute candidate values of z and v when range [1..i] is partitioned into [1..j] and [j + 1..i].
             z_j = max(z_old, z_new)
             v_j = v_old + new_demand*v_new + max((z_old - z_new)*new_demand, (z_new - z_old)*old_demand)
-            s_j = pairs[j, k-1]['s'].copy() + [j+1]
-            e_j = pairs[j, k-1]['e'].copy() + [i]
             # dominance check
             q = z_j - v_j/total_demand
             if q < best_diff:
                 best_diff = q
                 min_j = j
-                candidate_labels = [z_j, v_j, s_j, e_j]
+                candidate_labels = [z_j, v_j]
             printv(f"\t\twith j={j}, v:{v_j} z:{z_j} q: {q}")
 
         printv(f"\tNon dominated solution with j:{min_j}\n")
