04b540db953b4cf9533b42c8423ba728dc4f85ec
Nicolas Facchinetti <facchinetti.nicolas@outlook.it>
2022-06-14 19:42:15 +0200

    print to file results

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import datetime
 
+file_res = 'res.txt'
+
 c1 = "order_no_price_constraint.py"
 c2 = "order_price_constraint.py"
 c3 = "no_order_price_constraint.py"
-c = [c1, c2, c3]
+c = [c1, c2]
 
 inst = "test_algo/{i}/{i}_{c}.ini"
 
@@ -16,10 +18,14 @@ i += [inst.format(i=800, c=c) for c in [20,30,40]]
 instances = [(version , instance) for version in c for instance in i ]
 res = {}
 for c, i in instances:
-    print(f'Case {c} instance {i}')
+    case_name = f'Case {c} instance {i}'
+    print(case_name)
     start = datetime.datetime.now()
     os.system(f"python {c} {i}")
     end = datetime.datetime.now()
     comp = end-start
     res[(c,i)] = comp
-    print(f"\nSecond elapsed: {comp.total_seconds()}\n")
+    result  = f"\nSecond elapsed: {comp.total_seconds()}\n"
+    print(result)
+    with open(file_res, 'a') as f:
+        f.write(case_name + result)
